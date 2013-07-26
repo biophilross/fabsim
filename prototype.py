d2042ed6e6da9b0d1eef4db3f03a1fca3693e43b
Phil Ross <philippross369@gmail.com>
2013-07-26 17:38:39 -0400

    added numConsumers as a simulation parameter, removed factories, fabricatores, and both methods and readded a universal run method

--- prototype.py
+++ prototype.py
@@ -54,14 +54,14 @@ class Producer:
   #inventory (Array of Goods)
   #rate (int)
   #id (string)
-  def __init__(self, inventory, rate, id):
+  def __init__(self, inventory, rate, idInput):
     self.inventory = inventory
     self.rate = rate
-    self.id = id
+    self.producerID = idInput
     self.profits = 0
 
   def getID(self):
-    return self.id
+    return self.producerID
 
   def getInventory(self):
     return self.inventory
@@ -94,7 +94,7 @@ class Producer:
     for good in self.inventory:
       good.incrementQuantityBy(self.rate)
 
-  def toString(self):
+  def __str__(self):
     for good in self.inventory:
       print "GoodID: " + str(good.getID()) + "   Price: " + str(good.getPrice()) + "  Quantity: " + str(good.getQuantity()) + "\n"
 
@@ -109,12 +109,13 @@ class simulation:
   #percentFactory (float) between 0 and 1
   #factoryRate (int)
   #fabricatoryRate (int)
-  def __init__(self, simLength, numGoods, numConsumers, percentFactory, factoryRate, fabricatorRate):
+  def __init__(self, simLength, numGoods, numConsumers, numProducers, percentFactory, factoryRate, fabricatorRate):
     print "Running..."
     print ""
     self.simLength = simLength
     self.numGoods = numGoods
     self.numConsumers = numConsumers
+    self.numProducers = numProducers
     self.numFactoryGoods = int(numGoods * percentFactory)
     self.numFabricatorGoods = numGoods - self.numFactoryGoods
     self.factoryRate = factoryRate
@@ -168,15 +169,14 @@ class simulation:
       bestProducer.sell(bestGood)
 
 ###############################################################################
-# PLOTTING METHOD
+# PLOTTING METHODs
 ###############################################################################
 
-  #times (Array of Timesteps)
-  #profits1 (Array of Profits from 1st Producer)
-  #profits2 (Array of Profits from 2nd Producer)
-  def plot(self, times, profits1, profits2):
-    plot1 = plt.plot(times, profits1)
-    plot2 = plt.plot(times, profits2)
+  #timesteps (Array of timesteps)
+  #producers (Dictionary of profits)
+  def plot_average_price(self, timesteps, producers):
+
+    plots = [plt.plot(timesteps, profits) for profits in profits.values()]
     plt.legend( ('Producer 1', 'Producer 2'), loc=2)
 
     axes = plt.gca()
@@ -187,170 +187,83 @@ class simulation:
     plt.show()
 
 ###############################################################################
-# JUST FACTORIES
+# RUN METHOD
 ###############################################################################
 
-  def factories(self): # eventually add numProducers
-    # factory = Producer(self.numFactoryGoods, self.factoryRate)
-    # producers = [factory1, factory2]
+  def run(self, scenario): 
+    # scenario = 'factories', 'fabricators' or 'all'
 
-    # intitialize arrays to plot
-    times = np.array(range(self.simLength + 1))
-    profits1 = np.zeros(self.simLength + 1)
-    profits2 = np.zeros(self.simLength + 1)
-    goodsDemanded = []
+    # initialize variables based on scenario input
+    if scenario == 'factories':
+      rate    = self.factoryRate
+      goods   = self.numFactoryGoods
+      localID = 'factory'
+    elif scenario == 'fabricators':
+      rate    = self.fabricatorRate
+      goods   = self.numFabricatorGoods
+      localID = 'fabricator'
+    elif scenario == 'all':
+      rate    = 0
+      goods   = 0
+      localID = 'idk'
 
-    # set inventories
-    inventories = [
-    [Good(rd.random(), rd.random(), self.factoryRate) for good in range(self.numFactoryGoods)], 
-    [Good(rd.random(), rd.random(), self.factoryRate) for good in range(self.numFactoryGoods)]
-    ]
-
-    # initialize producers
-    factory1 = Producer(inventories[0], self.factoryRate, 'Factory1')
-    factory2 = Producer(inventories[1], self.factoryRate, 'Factory2')
-    producers = [factory1, factory2]
+    # initialize producers and arrays for plotting
+    producers = []
+    profits = dict()
+    for i in range(self.numProducers):
+      inventory = [Good(rd.random(), rd.random(), rate) for good in range(goods)] # set inventory
+      key = localID + str(i) # set key and id
+      producers.append(Producer(inventory, rate, key))
+      profits[key] = np.zeros(self.simLength + 1)
 
     #run simulations
+    goodsDemanded = [] # keeping track of goods demanded
     for timestep in range(self.simLength):
-      print str(timestep),
+      print str(timestep), # print timesteps to console to observe progress
       for numConsumers in range(self.numConsumers):
         goodDemanded = rd.random()
         goodsDemanded.append(goodDemanded)
-        self.rouletteConsumerBuysFrom(producers, goodDemanded)
+        self.consumerBuysFrom(producers, goodDemanded)
       for producer in producers:
         producer.updateInventory()
-      profits1[timestep + 1] = factory1.getProfits()
-      profits2[timestep + 1] = factory2.getProfits()
-
-    averageGoodDemanded = sum(goodsDemanded) / len(goodsDemanded)
+        profits[producer.getID()][timestep + 1] = producer.getProfits()
 
     # display winner
     print "\n"
     print "=================================================="
-    if factory1.getProfits() > factory2.getProfits():
-      print factory1.getID() + " Wins!\n"
-    else:
-      print factory2.getID() + " Wins!\n"
+    print max(profits, key = lambda key: sum(profits[key])) + " Wins!\n"
 
     # display stats
-    print factory1.getID() + " Profits: " + str(factory1.getProfits())
-    print factory1.getID() + " Average Price: " + str(factory1.getAverageGoodPrice())
-    print factory1.getID() + " Average Good Distance: " + str(abs(factory1.getAverageGoodID() - averageGoodDemanded))
-    #factory1.toString()
-    print ""
-    print factory2.getID() + " Profits: " + str(factory2.getProfits())
-    print factory2.getID() + " Average Price: " + str(factory2.getAverageGoodPrice())
-    print factory2.getID() + " Average Good Distance: " + str(abs(factory2.getAverageGoodID() - averageGoodDemanded))
+    for producer in producers:
+      print producer.getID() + " Profits: " + str(producer.getProfits())
+      print producer.getID() + " Average Price: " + str(producer.getAverageGoodPrice())
+      print producer.getID() + " Average Good Distance: " + str(abs(producer.getAverageGoodID() - (sum(goodsDemanded) / len(goodsDemanded))))
+      print ""
     print "================================================="
-    #factory2.toString()
-    # plot results
-    #self.plot(times, profits1, profits2)
 
-###############################################################################
-# JUST FABRICATORES
-###############################################################################
+    #Does the producer with the most profits in the end also have the highest average price?
+    averagePrices = dict()
+    for producer in producers:
+      averagePrices[producer.getID()] = producer.getAverageGoodPrice()
 
-  def fabricators(self): # eventually add numProducers
-    # fabricator = Producer(self.numFabricatorGoods, self.fabricatorRate)
-    # producers = [fabricator1, fabricator2]
-
-    # initialize arrays to plot
-    times = np.array(range(self.simLength + 1))
-    profits1 = np.zeros(self.simLength + 1)
-    profits2 = np.zeros(self.simLength + 1)
-
-    # set inventories
-    inventories = [
-    [Good(rd.random(), rd.random(), self.fabricatorRate) for good in range(self.numFabricatorGoods)], 
-    [Good(rd.random(), rd.random(), self.fabricatorRate) for good in range(self.numFabricatorGoods)]
-    ]
-
-    fabricator1 = Producer(inventories[0], self.fabricatorRate, 'Fabricator1')
-    fabricator2 = Producer(inventories[1], self.fabricatorRate, 'Fabricator2')
-    producers = [fabricator1, fabricator2]
-
-    for timestep in range(self.simLength):
-      print str(timestep),
-      print ""
-      for numConsumers in range(self.numConsumers):
-        self.consumerBuysFrom(producers)
-      for producer in producers:
-        producer.updateInventory()
-      profits1[timestep + 1] = fabricator1.getProfits()
-      profits2[timestep + 1] = fabricator2.getProfits()
-
-    if fabricator1.getProfits() > fabricator2.getProfits():
-      print "Fabricator1 Wins!\n"
+    if max(averagePrices, key = lambda key: averagePrices[key]) == max(profits, key = lambda key: sum(profits[key])):
+      result = 1 # they both have the most profits and highest average good price
     else:
-      print "Fabricator2 Wins!\n"
-    print "Fabricator1 Profits: " + str(fabricator1.getProfits())
-    print "Fabricator1 Average Price: " + str(fabricator1.getAverageGoodPrice())
-    #fabricator1.toString()
-    print ""
-    print "Fabricator2 Profits: " + str(fabricator2.getProfits())
-    print "Fabricator2 Average Price: " + str(fabricator2.getAverageGoodPrice())
-    #fabricator2.toString()
-    #self.plot(times, profits1, profits2)
+      result = 0 # they don't
 
-###############################################################################
-# FACTORIES & FABRICATORS
-###############################################################################
-
-  def factoriesAndFabricators(self): # eventually add numProducers
-    # factory = Producer(self.numFactoryGoods, self.factoryRate)
-    # fabricator = Producer(self.numFabricatorGoods, self.fabricatorRate)
-    # producers = [factory, fabricator]
-
-    # initialize arrays to plot
-    times = np.array(range(self.simLength + 1))
-    profits1 = np.zeros(self.simLength + 1)
-    profits2 = np.zeros(self.simLength + 1)
-
-    # set inventories
-    inventories = [
-    [Good(rd.random(), rd.random(), self.factoryRate) for good in range(self.numFactoryGoods)], 
-    [Good(rd.random(), rd.random(), self.fabricatorRate) for good in range(self.numFabricatorGoods)]
-    ]
-
-    factory = Producer(inventories[0], self.factoryRate, 'Factory')
-    fabricator = Producer(inventories[1], self.fabricatorRate, 'Fabricator')
-    producers = [factory, fabricator]
-
-    for timestep in range(self.simLength):
-      print str(timestep),
-      print ""
-      for numConsumers in range(self.numConsumers):
-        self.consumerBuysFrom(producers)
-      for producer in producers:
-        producer.updateInventory()
-      profits1[timestep + 1] = factory.getProfits()
-      profits2[timestep + 1] = fabricator.getProfits()
-
-    if factory.getProfits() > fabricator.getProfits():
-      print factory.getID() + " Wins!\n"
-    else:
-      print fabricator.getID() + " Wins!\n"
-    print factory.getID() + " Profits: " + str(factory.getProfits())
-    print factory.getID() + " Average Price: " + str(factory.getAverageGoodPrice())
-    #factory.toString()
-    print ""
-    print fabricator.getID() + " Profits: " + str(fabricator.getProfits())
-    print fabricator.getID() + " Average Price: " + str(fabricator.getAverageGoodPrice())
-    print ""
-    #fabricator.toString()
-    #self.plot(times, profits1, profits2)
+    return result
 
 ###############################################################################
 # RUN SIM
 ###############################################################################
 
-SIMLENGTH = 100
+SIMLENGTH = 10
 NUMGOODS = 100
 NUMCONSUMERS = 1000
+NUMPRODUCERS = 2
 PERCENTFACTORY = 0.1
-FACTORYRATE = 100
-FABRICATORRATE = 10
-sim = simulation(SIMLENGTH, NUMGOODS, NUMCONSUMERS, PERCENTFACTORY, FACTORYRATE, FABRICATORRATE)
+FACTORYRATE = 10
+FABRICATORRATE = 1
+sim = simulation(SIMLENGTH, NUMGOODS, NUMCONSUMERS, NUMPRODUCERS, PERCENTFACTORY, FACTORYRATE, FABRICATORRATE)
 
-sim.factories()
+sim.run('factories')
